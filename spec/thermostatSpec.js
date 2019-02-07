3631bd8c65c1e43d3af0b937192c7a37a5094bb2
Brooke Woolley <brooke.woolley@gmail.com>
2019-02-07 14:36:18 +0000

    Passing tests for increasing and decreasing temperature

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,22 @@
+describe('Thermostat', function(){
+  var thermostat;
+
+  beforeEach(function(){
+  thermostat = new Thermostat();
+});
+
+  it('starts at 20 degrees', function(){
+  expect(thermostat.getCurrentTemperature()).toEqual(20);
+});
+
+  it('increases in temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with down()', function() {
+    thermostat.up();
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  })
+});
